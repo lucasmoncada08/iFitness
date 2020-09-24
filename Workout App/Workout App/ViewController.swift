15fa53e7717c04bd10662b969bf2ade960229e7f
Lucas Moncada <42893974+lucasmoncada08@users.noreply.github.com>
2020-09-24 17:42:45 -0400

    Added Starting Files & Basic Visual

---
+++ Workout App/Workout App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Workout App
+//
+//  Created by Lucas Moncada on 2020-09-24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
