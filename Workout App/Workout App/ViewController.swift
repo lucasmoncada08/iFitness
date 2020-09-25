5c30c53a0ae30f64c1b12c08e05ad825a05a7ce0
Lucas Moncada <lucas.moncada08@gmail.com>
2020-09-25 00:34:32 -0400

    Adding Progress Bar

--- Workout App/Workout App/ViewController.swift
+++ Workout App/Workout App/ViewController.swift
@@ -9,11 +9,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var progressBar: UIProgressView!
+    
+    var forwardProgress: Bool = true
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    @objc func checkForm() {
+        if progressBar.progress < 1.0 && forwardProgress {
+            progressBar.progress += 0.02
+            if progressBar.progress == 1.0 {
+                forwardProgress = false
+            }
+        }
+        
+        if progressBar.progress > 0.0 && !forwardProgress {
+            progressBar.progress -= 0.02
+            if progressBar.progress == 0.0 {
+                forwardProgress = true
+            }
+        }
+    }
+    
+    @IBAction func correctForm(_ sender: Any) {
+        Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(checkForm), userInfo: nil, repeats: true)
+    }
+    
 }
 
